89cd345a34256defceffe42d5cf6c2b75954868b
Nurullah Eren Acar <ilkemirnurullah@gmail.com>
2024-08-19 15:10:32 +0300

    version 11

--- mavlinkHandler/mavlinkHandler.py
+++ mavlinkHandler/mavlinkHandler.py
@@ -3,75 +3,80 @@ from pymavlink.quaternion import QuaternionBase
 import time
 import math
 import geopy.distance
-from dronekit import connect, VehicleMode, LocationGlobalRelative
+from dronekit import connect, VehicleMode, LocationGlobalRelative, Command
 
 
 class MAVLinkHandlerDronekit:
     """
     init by: mavlink_handler = MAVLinkHandler(f'127.0.0.1:{port}')
     """
-    def __init__(self, connection_string, distance_radius=15):
-        self.master = connect(connection_string, wait_ready=True)
+    def __init__(self, connection_string, _wait_ready=False, refresh_rate=50):
+        self.master = connect(connection_string, wait_ready=_wait_ready, rate=refresh_rate)
         self.boot_time = time.time()
-        self.radians_to_degrees = lambda radians: radians * (180.0 / math.pi)
-        self.DISTANCE_RADIUS = distance_radius 
 
+    def arm_vehicle(self):
+        self.master.armed = True
 
-    def to_quaternion(self, roll = 0.0, pitch = 0.0, yaw = 0.0):
-        """
-        Convert degrees to quaternions
-        """
-        t0 = math.cos(math.radians(yaw * 0.5))
-        t1 = math.sin(math.radians(yaw * 0.5))
-        t2 = math.cos(math.radians(roll * 0.5))
-        t3 = math.sin(math.radians(roll * 0.5))
-        t4 = math.cos(math.radians(pitch * 0.5))
-        t5 = math.sin(math.radians(pitch * 0.5))
+    def disarm_vehicle(self):  
+        self.master.armed = False
 
-        w = t0 * t2 * t4 + t1 * t3 * t5
-        x = t0 * t3 * t4 - t1 * t2 * t5
-        y = t0 * t2 * t5 + t1 * t3 * t4
-        z = t1 * t2 * t4 - t0 * t3 * t5
-
-        return [w, x, y, z]
-
-    def set_target_attitude(self, roll = 0.0, pitch = 0.0,
-                         yaw = None, yaw_rate = 0.0, use_yaw_rate = False,
-                         thrust = 0.5):
-        """
-        use_yaw_rate: the yaw can be controlled using yaw OR yaw_rate.
-                    When one is used, the other is ignored by Ardupilot.
-        thrust: 0 <= thrust <= 1, as a fraction of maximum vertical thrust.
-                Note that as of Copter 3.5, thrust = 0.5 triggers a special case in
-                the code for maintaining current altitude.
-        """
-        if yaw is None:
-            # this value may be unused by the vehicle, depending on use_yaw_rate
-            yaw = self.master.attitude.yaw
-
-        # Thrust >  0.5: Ascend
-        # Thrust == 0.5: Hold the altitude
-        # Thrust <  0.5: Descend
-        msg = self.master.message_factory.set_attitude_target_encode(
-            0, # time_boot_ms
-            1, # Target system
-            1, # Target component
-            0b00000000 if use_yaw_rate else 0b00000100,
-            self.to_quaternion(roll, pitch, yaw), # Quaternion
-            0, # Body roll rate in radian
-            0, # Body pitch rate in radian
-            math.radians(yaw_rate), # Body yaw rate in radian/second
-            thrust  # Thrust
-        )
-        
-        self.master.send_mavlink(msg)
+    def takeoff(self, target_altitude):
+        self.master.mode = VehicleMode("TAKEOFF")
 
     def set_mode(self, mode_name):
         self.master.mode = VehicleMode(mode_name)
 
-    def get_attitude(self):
-        return self.radians_to_degrees(self.master.attitude.roll), self.radians_to_degrees(self.master.attitude.pitch), self.radians_to_degrees(self.master.attitude.yaw)
+    def set_parameter_value(self, parameter_name, value):
+        self.master.parameters[parameter_name] = value
+
+    def set_target_attitude(self, roll, pitch, yaw, thrust, roll_rate=0, pitch_rate=0, yaw_rate=0):
+        msg = self.master.message_factory.set_attitude_target_encode(
+            int(1e3 * (time.time() - self.boot_time)),
+            self.master._master.target_system, self.master._master.target_component,
+            0,
+            QuaternionBase([math.radians(angle) for angle in (roll, pitch, yaw)]),
+            roll_rate, 
+            pitch_rate, 
+            yaw_rate, 
+            thrust
+        )
         
+        self.master.send_mavlink(msg)
+
+    def get_attitude(self):
+        return math.degrees(self.master.attitude.roll), math.degrees(self.master.attitude.pitch), math.degrees(self.master.attitude.yaw)
+    
+    def get_battery(self):
+        """
+        Returns a tuple with the battery voltage and the battery level
+        Returns:
+            tuple: (voltage, level)
+        """
+        return self.master.battery.voltage, self.master.battery.level
+    
+    def get_lidar_distance(self):
+        return self.master.rangefinder.distance
+    
+    def get_mode_mapping(self):
+        return self.master._mode_mapping
+    
+    def get_parameter_value(self, parameter_name):
+        return self.master.parameters[parameter_name]
+    
+    def get_mode(self):
+        return self.master.mode.name
+    
+    def get_heading(self):
+        return self.master.heading
+    
+    def get_air_speed(self):
+        return self.master.airspeed
+    
+    def get_ground_speed(self):
+        return self.master.groundspeed
+    
+    def get_location(self):
+        return self.master.location.global_frame.lat, self.master.location.global_frame.lon, self.master.location.global_frame.alt
 
     def simple_go_to(self, lat, lon, alt, block=False, distance_radius=15):
 
@@ -89,45 +94,78 @@ class MAVLinkHandlerDronekit:
                 if distance <= distance_radius:
                     break
 
-    def return_master(self):
-        return self.master
+    def clear_mission(self):
+        cmds = self.master.commands
+        cmds.clear()
+        self.master.commands.upload()
 
+    def readmission(self, aFileName):
+        cmds = self.master.commands
+        missionlist=[]
+        with open(aFileName) as f:
+            for i, line in enumerate(f):
+                if i==0:
+                    if not line.startswith('QGC WPL 110'):
+                        raise Exception('File is not supported WP version')
+                else:
+                    linearray=line.split('\t')
+                    ln_index=int(linearray[0])
+                    ln_currentwp=int(linearray[1])
+                    ln_frame=int(linearray[2])
+                    ln_command=int(linearray[3])
+                    ln_param1=float(linearray[4])
+                    ln_param2=float(linearray[5])
+                    ln_param3=float(linearray[6])
+                    ln_param4=float(linearray[7])
+                    ln_param5=float(linearray[8])
+                    ln_param6=float(linearray[9])
+                    ln_param7=float(linearray[10])
+                    ln_autocontinue=int(linearray[11].strip())
+                    cmd = Command( 0, 0, 0, ln_frame, ln_command, ln_currentwp, ln_autocontinue, ln_param1, ln_param2, ln_param3, ln_param4, ln_param5, ln_param6, ln_param7)
+                    missionlist.append(cmd)
+        return missionlist
 
+    def upload_mission(self, file_path):
+        missionlist = self.readmission(file_path)
+        cmds = self.master.commands
+        cmds.clear()
 
+        for command in missionlist:
+            cmds.add(command)
 
-    def get_location(self):
-        return self.master.location.global_frame.lat, self.master.location.global_frame.lon, self.master.location.global_frame.alt
-
-    def get_mode(self):
-        return self.master.mode.name
+        self.master.commands.upload()
+        
     
-    def get_heading(self):
-        return self.master.heading
-    
-    def get_air_speed(self):
-        return self.master.airspeed
-    
-    def is_armed(self):
-        return self.master.armed
-    
-    def is_armable(self):
-        return self.master.is_armable
+
 
 class MAVLinkHandlerPymavlink:
-    def __init__(self, connection_string, distance_radius=15):
-        self.master = mavutil.mavlink_connection(connection_string)
+    def __init__(self, connection_string, _autoreconnect=False, message_hz=50):
+        self.master = mavutil.mavlink_connection(connection_string, autoreconnect=_autoreconnect)
         self.master.wait_heartbeat()
         self.boot_time = time.time()
-        self.radians_to_degrees = lambda radians: radians * (180.0 / math.pi)
-        self.DISTANCE_RADIUS = distance_radius 
 
-    def set_target_attitude(self, roll, pitch, yaw, thrust):
+        used_messages = [mavutil.mavlink.MAVLINK_MSG_ID_HEARTBEAT, mavutil.mavlink.MAVLINK_MSG_ID_ATTITUDE, mavutil.mavlink.MAVLINK_MSG_ID_GLOBAL_POSITION_INT]
+        self.request_message_interval(used_messages, message_hz)
+    
+    def request_message_interval(self, message_list, frequency_hz):
+        for message_input in message_list:
+            self.master.mav.command_long_send(
+            self.master.target_system, self.master.target_component,
+            mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL, 0,
+            message_input,
+            1e6 / frequency_hz,
+            0, 0, 0, 0, 0)
+
+    def set_target_attitude(self, roll, pitch, yaw, thrust, roll_rate=0, pitch_rate=0, yaw_rate=0):
         self.master.mav.set_attitude_target_send(
             int(1e3 * (time.time() - self.boot_time)),
             self.master.target_system, self.master.target_component,
             0,
             QuaternionBase([math.radians(angle) for angle in (roll, pitch, yaw)]),
-            0, 0, 0, thrust
+            roll_rate, 
+            pitch_rate, 
+            yaw_rate, 
+            thrust
         )
 
     def set_mode(self, mode_name):
@@ -135,11 +173,9 @@ class MAVLinkHandlerPymavlink:
         self.master.set_mode(mode_id)
 
     def get_attitude(self):
-        self.master.mav.request_data_stream_send(self.master.target_system, self.master.target_component,
-                                         mavutil.mavlink.MAV_DATA_STREAM_EXTRA1, 10, 1)
         msg = self.master.recv_match(type='ATTITUDE', blocking=True)
         if msg is not None:
-            return self.radians_to_degrees(msg.pitch), self.radians_to_degrees(msg.roll), self.radians_to_degrees(msg.yaw)
+            return math.degrees(msg.pitch), math.degrees(msg.roll), math.degrees(msg.yaw)
         
     def simple_goto(self, lat , lon, alt, block=False, distance_radius=15):
 
@@ -161,8 +197,6 @@ class MAVLinkHandlerPymavlink:
                     break
 
     def get_location(self):
-        self.master.mav.request_data_stream_send(self.master.target_system, self.master.target_component,
-                                         mavutil.mavlink.MAV_DATA_STREAM_EXTRA1, 30, 1)
         msg = self.master.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
         if msg is not None:
             return msg.lat/1.0e7, msg.lon/1.0e7, msg.alt/1000.0
@@ -188,20 +222,6 @@ class MAVLinkHandlerPymavlink:
 
 if __name__ == '__main__':
     """Test"""
-    mavlink_handler = MAVLinkHandler('127.0.0.1:14551')
-
-    print("Change mode to GUIDED")
-    mavlink_handler.set_mode('GUIDED')
-
-
-    print("Location:", mavlink_handler.get_location())
-    print("Mode:", mavlink_handler.get_mode())
-    print("Heading:", mavlink_handler.get_heading())
-    print("Air Speed:", mavlink_handler.get_air_speed())
-    print("Armed:", mavlink_handler.is_armed())
-    print("Armable:", mavlink_handler.is_armable())
-
-        
-        
-        
+    mavlink_handler = MAVLinkHandlerDronekit('udp:127.0.0.1:14591',_wait_ready=False)
     
+    mavlink_handler.upload_mission("Wp_Uploader/wp_type8")
